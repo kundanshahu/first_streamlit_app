5049ce7383f827b0b421877580884253ab5d6fd3
kundanshahu <96973428+kundanshahu@users.noreply.github.com>
2023-08-27 22:50:38 +0530

    pretty printing json data response

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# Normalizing / pretty printing the response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# write into dataframe and display
+streamlit.dataframe(fruityvice_normalized)
