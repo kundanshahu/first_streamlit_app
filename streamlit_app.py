122a51ad5fb573cfee2b065bd7dc526dca2d5aa5
kundanshahu <96973428+kundanshahu@users.noreply.github.com>
2023-08-31 15:06:20 +0530

    Query data from snowflake tbl

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,3 +39,9 @@ my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
+#-----------------
+# Querying some data from table snowflake
+my_cur.execute("SELECT * from PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST")
+my_data_row = my_cur.fetchone()
+streamlit.text("The fruit load list contains : ")
+streamlit.text(my_data_row)
