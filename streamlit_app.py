3a50e34f0ff7505d55187c46e34736746e636c08
kundanshahu <96973428+kundanshahu@users.noreply.github.com>
2023-08-31 16:12:50 +0530

    adding a function for insert into snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,6 +57,23 @@ if streamlit.button("Get Fruit Load List"):
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
 
+streamlit.stop()
+
+# -- Adding a new fruit from user prompt
+
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES ('from streamlit')")
+    return "Thanks for adding " + new_fruit
+
+my_fruit_add = streamlit.text_input("What fruit would you like to add?", 'Jackfruit')
+
+if streamlit.button("Add a Fruit to the list"):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(my_fruit_add)
+  streamlit.text(back_from_function)
+
+
 # my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 # my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -64,9 +81,3 @@ if streamlit.button("Get Fruit Load List"):
 # streamlit.text("Hello from Snowflake:")
 # streamlit.text(my_data_row)
 
-#-----------------
-# -- Adding a new fruit from user prompt
-my_fruit_add = streamlit.text_input("What fruit would you like to add?", 'Jackfruit')
-streamlit.write("Thanks for adding ", my_fruit_add)
-
-# my_cur.execute("INSERT INTO PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST VALUES :my_fruit_add")
